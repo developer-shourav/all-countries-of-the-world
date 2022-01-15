951f5bdd8236104bbc5fba9c165bdbf18d8d6e4f
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-01-15 15:24:07 +0600

    primary porject demo complited

--- countries.js
+++ countries.js
@@ -7,7 +7,27 @@ const loadCountries = () => {
 };
 
 
+
 const displayCountries = (countries) => {
-    console.log(countries);
+    console.log(countries[0]);
+    const container = document.getElementById('countries');
+
+    const countryHTML = countries.map(country =>getCountryHTML(country) );
+    container.innerHTML = countryHTML.join(" ");
+
+   
 
 }
+
+const getCountryHTML = country => {
+    return `
+     <div class="country">
+        <h2> ${country.name.common} </h2> 
+        <h4> Capital: ${country.capital} </h4>
+        <img src="${country.flags.png}" > 
+     </div>
+
+    `
+}
+
+loadCountries();
